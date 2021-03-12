f14b2412032bb4ed25bd37d376c19d26618f5865
georgeoi <--list>
2021-03-12 16:51:55 -0300

    reuelto

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const challengeFunction = function(cell1, cell2){
-    /* YOUR CODE HERE */
-    let result
-
-    return result
+    let firstLetter = cell1.split("")[0]
+    let firsttNumber = cell1.split("")[1]
+    let secondLetter = cell2.split("")[0]
+    let secondNumber = cell2.split("")[1]
+    const letterArray= ["A","B","C","D","E","F","G","H"]
+    let movimientos1= letterArray.indexOf(firstLetter)-letterArray.indexOf(secondLetter)
+    let movimientos2= firsttNumber-secondNumber
+    let mov = movimientos1+movimientos2
+    return mov%2==0 
 }
-
 module.exports = challengeFunction
